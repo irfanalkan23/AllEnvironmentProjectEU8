7fc290c4c6d0f0c973f1414151299be0412128a5
OscarSah <oscar@cybertekschool.com>
2022-07-16 21:45:22 +0300

    Steps created

---
+++ src/test/java/com/cydeo/step_definitions/bookITSteps.java
@@ -0,0 +1,30 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class bookITSteps {
+
+    @Given("User logs into UI app with {string} and {string}")
+    public void user_logs_into_ui_app_with_and(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User navigates to mySelf page and gets user info")
+    public void user_navigates_to_my_self_page_and_gets_user_info() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User sends a query to bookIT DB with {string}")
+    public void user_sends_a_query_to_book_it_db_with(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("UI and DB information should match")
+    public void ui_and_db_information_should_match() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ src/test/resources/features/bookIT.feature
@@ -0,0 +1,7 @@
+Feature: BookIT application UI and DB end to end testing
+  @wip
+  Scenario: UI and DB user verification
+    Given User logs into UI app with "email" and "password"
+    When User navigates to mySelf page and gets user info
+    And User sends a query to bookIT DB with "email"
+    Then UI and DB information should match
