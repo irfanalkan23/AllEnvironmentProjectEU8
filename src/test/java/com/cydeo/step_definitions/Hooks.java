b3e78bd73ec99b67516ce9ddbd9dc1d09acdeb98
OscarSah <oscar@cybertekschool.com>
2022-07-16 22:00:14 +0300

    Steps created

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,6 +1,18 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.DBUtils;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
 public class Hooks {
 
+    @Before("@db")
+    public void dbStart(){
+        DBUtils.createConnection();
+    }
 
+    @After("@db")
+    public void dbEnd(){
+        DBUtils.destroy();
+    }
 }
