6fcc494f6e18d69c4892b02a66fff2db99c83dc4
OscarSah <oscar@cybertekschool.com>
2022-07-30 19:41:50 +0300

    Utility Updated

--- src/test/java/com/cydeo/tests/BookItAPIvsDBwithParameterized.java
+++ src/test/java/com/cydeo/tests/BookItAPIvsDBwithParameterized.java
@@ -1,6 +1,12 @@
 package com.cydeo.tests;
 
+import com.cydeo.utilities.ExcelUtil;
+
+import java.util.List;
+import java.util.Map;
+
 public class BookItAPIvsDBwithParameterized {
+
 
 
 
--- src/test/java/com/cydeo/utilities/BookItUtils.java
+++ src/test/java/com/cydeo/utilities/BookItUtils.java
@@ -3,6 +3,8 @@ package com.cydeo.utilities;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
+import java.util.List;
+import java.util.Map;
 
 
 public class BookItUtils {
@@ -15,5 +17,10 @@ public class BookItUtils {
                 .header("Authorization", Token.getToken())
                 .when()
                 .get(path).path(responsePath);
+    }
+
+    public static List<Map<String ,String >> getExcelData(){
+        ExcelUtil bookitFile = new ExcelUtil("src/test/resources/BookItQa3.xlsx","QA3");
+        return bookitFile.getDataList();
     }
 }
